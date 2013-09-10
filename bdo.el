6ed185c66a3f61912de839d129cc6ad45d2bdf54
Chris Done <chrisdone@gmail.com>
2013-09-10 10:26:43 +0200

    Make library, bump version

--- bdo.el
+++ bdo.el
@@ -4,7 +4,7 @@
 
 ;; Author:   Chris Done <chrisdone@gmail.com>
 ;; Created:  07-April-2012
-;; Version:  0.1
+;; Version:  0.2
 ;; Keywords: development
 
 ;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
@@ -22,236 +22,28 @@
 ;;
 ;;; Commentary:
 ;;
-;; Usage:
-;;
-;; (add-to-list 'load-path "/the/path/to/bdo")
-;;
-;; Optional keybinding:
-;;
-;; (define-key css-mode-map (kbd "C-x C-s") 'css-refresh)
-;; (defun css-refresh ()
-;;   "Refresh the current CSS file."
-;;   (interactive)
-;;   (when (buffer-modified-p)
-;;     (save-buffer))
-;;   (bdo-refresh))
+;;; Usage:
 ;;
 
 ;;; Code:
 
 (require 'cl)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;  Customization
-
 (defcustom bdo-port
   9090
-  "The port to listen on (to which the browser connects)."
+  "The bdo port."
   :type 'integer
   :group 'bdo)
 
-(defcustom bdo-js-file
-  "bdo.js"
-  "The JS file to read (if cannot find absolute or relative to
-current buffer, tries to get from 'load-path."
-  :type 'string
-  :group 'bdo)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Types
-
-(defstruct
-  (bdo-client
-   (:constructor bdo-client-make))
-  id
-  links
-  process)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Globals
-
-(defvar bdo--clients nil)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Public functions
-
-(defun bdo-listen ()
-  "Start the listener (or: make sure it's started)."
+(defun bdo-refresh ()
   (interactive)
-  (bdo--get-listener)
-  (message "Listener started on port %d…" bdo-port))
+  (bdo-command "update"))
 
-(defun bdo-stop ()
-  "Stop listening."
-  (interactive)
-  (delete-process (bdo--get-listener))
-  (message "Listener stopped."))
+(defun bdo-clients ()
+  (let ((clients (split-string (bdo-command "clients")
+                               "\n\n")))))
 
-(defun bdo-refresh (&optional href)
-  "Refresh the current stylesheet."
-  (interactive)
-  (let* ((client (bdo-client))
-         (process (bdo-client-process client))
-         (link (and process (bdo-link))))
-    (when (and process link)
-      (bdo--reply-plain (bdo-client-process client) link)
-      (message "Refreshed."))))
-
-(defun bdo-link ()
-  "Get the current stylesheet."
-  (or (if (boundp 'bdo-link)
-          bdo-link
-        (set (make-local-variable 'bdo-link) nil))
-      (bdo-set-link)))
-
-(defun bdo-set-link ()
-  "Set the current stylesheet."
-  (interactive)
-  (setq bdo-link
-        (ido-completing-read "Stylesheet: " (bdo-client-links (bdo-client)))))
-
-(defun bdo-client ()
-  "Get the current client."
-  (or (if (boundp 'bdo-client)
-          (find-if (lambda (client) (string= (bdo-client-id client) bdo-client))
-                   bdo--clients)
-        (set (make-local-variable 'bdo-client) nil))
-      (progn (bdo-set-client)
-             (bdo-client))))
-
-(defun bdo-set-client ()
-  "Set the current client."
-  (interactive)
-  (if (consp bdo--clients)
-      (let ((id (ido-completing-read "Client: " (mapcar 'bdo-client-id bdo--clients))))
-        (setq bdo-client id))
-    (error "There are no current clients!")))
-
-(defun bdo-clear-clients ()
-  "Clear the clients."
-  (interactive)
-  (setq bdo--clients nil)
-  (setq bdo-link nil)
-  (message "Clients cleared."))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Private functions
-
-(defun bdo--get-listener ()
-  "Get the existing listener or make it."
-  (let ((name "*bdo-listener*"))
-    (or (get-process name)
-        (make-network-process
-         :name name
-         :server t
-         :host nil
-         :service bdo-port
-         :family 'ipv4
-         :coding 'utf-8
-         :filter 'bdo--filter
-         :sentinel 'bdo--sentinel
-         :log 'bdo--log))))
-
-(defun bdo--filter (process data)
-  "Callback for INCOMMING data on a connection."
-  (when (string-match "\\(GET\\|POST\\) /\\(.+\\) " data)
-    (let ((cmd (match-string 2 data))
-          (referer (bdo--get-header data "Referer")))
-      (cond
-       ((string-match "^bdo" cmd)
-        (bdo--reply-js process (bdo--get-js (bdo--get-header data "Host")))
-        (if (bdo--find-client referer)
-            (message "Client re-connected: %s" referer)
-          (progn (add-to-list 'bdo--clients (bdo-client-make :id referer :process nil))
-                 (message "New client: %s" referer))))
-       ((string-match "^poll" cmd)
-        (let ((client (bdo--find-client referer)))
-          (setf (bdo-client-process client) process)))
-       ((string-match "^links" cmd)
-        (let ((client (bdo--find-client referer)))
-          (bdo--update-links client data)
-          (bdo--reply-plain process "Links updated.")))
-       (t
-        (bdo--reply-plain process (format "Unknown command: %S" cmd)))))))
-
-(defun bdo--find-client (referer)
-  "Find a client by the given referer."
-  (find-if (lambda (client)
-             (string= (bdo-client-id client) referer))
-           bdo--clients))
-
-(defun bdo--reply-js (process body)
-  "Reply with some JS."
-  (bdo--reply process body "text/javascript"))
-
-(defun bdo--reply-plain (process body)
-  "Reply with plain text."
-  (bdo--reply process body "text/plain"))
-
-(defun bdo--reply (process body content-type)
-  "Send a valid HTTP reply to the browser."
-  (process-send-string
-   process
-   (format "%s\n%s\n%s\n%s\n\n%s"
-           "HTTP/1.1 200 OK"
-           (format "Content-Type: %s" content-type)
-           "Access-Control-Allow-Origin: *"
-           (format "Content-Length: %s" (string-bytes body))
-           body))
-  (process-send-eof process))
-
-(defun bdo--sentinel (process status)
-  "Callback for the STATUS CHANGE of a connection.")
-
-(defun bdo--log (server client message)
-  "Callback to log the NEW CONNECTIONS of the listener.")
-
-(defun bdo--get-js (host)
-  "Get the JS code to send to the client."
-  (if (bdo--find-js-file)
-      (with-temp-buffer
-        (insert-file-contents (bdo--find-js-file))
-        (goto-char (point-max))
-        (insert (format "bdo.host = %S;\n" (format "http://%s/" host))
-                "bdo.init();")
-        (goto-char (point-max))
-        (buffer-substring-no-properties (point-min) (point-max)))
-    (error "Unable to find `bdo-js-file'.")))
-
-(defun bdo--find-js-file ()
-  "Find a path for the JS file that exists."
-  (let ((absolute-or-relative bdo-js-file))
-    (cond ((file-exists-p absolute-or-relative) absolute-or-relative)
-          (t (let ((choices (remove-if-not 'file-exists-p
-                                           (mapcar (lambda (dir)
-                                                     (concat dir "/" bdo-js-file))
-                                                   load-path))))
-               (when (consp choices)
-                 (car choices)))))))
-
-(defun bdo--get-header (data header)
-  "Get the given header."
-  (let ((value (and (string-match (format "^%s: \\(.+\\)$" header)
-                                  data)
-                    (match-string 1 data))))
-    (if (not value)
-        (message "Unable to get header %s from client, request was: %S." header data)
-      value)))
-
-(defun bdo--update-links (client data)
-  "Get the links from the request and update our internal list."
-  (let ((post-data (bdo--post-data data)))
-    (let ((links (and (string-match "^links=\\(.+\\)" post-data)
-                      (split-string (match-string 1 post-data) "\n"))))
-      (setf (bdo-client-links client) links))))
-
-(defun bdo--post-data (data)
-  "Get the post data from the request."
-  (let ((post-data (and (string-match "\n\n\\(.+\\)" data)
-                        (match-string 1 data))))
-    (if (not post-data)
-        (error "Unable to parse post data from request.")
-      (url-unhex-string (replace-regexp-in-string "+" "%20" post-data)))))
-
-(provide 'bdo)
-;;; bdo.el ends here
+(defun bdo-command (command)
+  (shell-command-to-string (format "echo %s | nc localhost %d"
+                                   command
+                                   bdo-port)))
---
+++ src/Bdo.hs
@@ -0,0 +1,144 @@
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+-- | Accepts polling requests and such.
+
+module Bdo
+  (startServer)
+  where
+
+import           Control.Concurrent
+import           Control.Exception
+import           Control.Monad
+import           Data.Aeson
+import           Data.List
+import           Data.Maybe
+import           Data.Monoid
+import           Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.IO as T
+import           Http
+import           Network
+import           Network.URL
+import           Paths_bdo
+import           Prelude hiding (catch)
+import           System.Environment
+import           System.IO
+
+startServer :: Int -> IO ()
+startServer listenPort = do
+  clients <- newMVar []
+  listener <- listenOn (PortNumber (fromIntegral listenPort))
+  currentClient <- newMVar Nothing
+  let printCurrentClient h = do
+         cur <- readMVar currentClient
+         case cur of
+           Nothing -> T.hPutStrLn h "No current client"
+           Just (client,link) -> T.hPutStrLn h $ "Current client is: " <> client <> ", updating link: " <> link
+      update h client link = do
+        clients <- readMVar clients
+        case lookup client clients of
+          Nothing -> T.hPutStrLn h "Unknown client. To see list of clients: clients"
+          Just (links,Just h')
+            | link `elem` links -> do T.hPutStrLn h "Sending link update ..."
+                                      reply h' [] link
+                                      hClose h'
+            | otherwise -> T.hPutStrLn h "That link doesn't exist in the page. To see the page's links: clients"
+          _ -> T.hPutStrLn h "That client isn't connected right now."
+      updateCurrentClient h = do client <- readMVar currentClient
+                                 case client of
+                                   Nothing -> hPutStrLn h "No current client!"
+                                   Just (client,link) -> update h client link
+      printClients h = do clients <- readMVar clients
+                          mapM_ (\(referer,(links,_)) -> do
+                           T.hPutStrLn h (referer <> ":\n" <>
+                                          T.intercalate "\n" (map ("  "<>) links) <>
+                                          "\n"))
+                            clients
+                          printCurrentClient h
+      setClient h client link = do
+        clients <- readMVar clients
+        case lookup client clients of
+          Nothing -> T.hPutStrLn h "No such client"
+          Just{} -> do modifyMVar_ currentClient (const (return (Just (client,link))))
+                       printCurrentClient h
+  void $ forkIO $ flip finally (sClose listener) $ forever $ do
+    (h,_,_) <- accept listener
+    let closing m = finally m (hClose h)
+    forkIO $ do
+      hSetBuffering h NoBuffering
+      headers <- getHeaders h
+      case headers of
+        ["update"] -> closing (updateCurrentClient h)
+        [T.words -> ["update",client,link]] -> update h client link
+        ["clients"] -> closing $ printClients h
+        [T.words -> ["set",client,link]] -> closing $ setClient h client link
+        [T.words -> [(importURL . T.unpack) -> Just client,(importURL . T.unpack) -> Just link]] -> closing $ do
+         T.putStrLn "Updating from socket request."
+         update h (T.pack (exportURL client))
+                  (T.pack (exportURL link))
+        _ -> do
+         case requestMethod headers of
+           Just (method,url) -> dispatch h clients method url headers
+           _ -> closing $ T.putStrLn $ "Request ignored: " <> T.pack (show headers)
+
+  forever $ do
+    line <- T.getLine
+    case T.words line of
+      ["clients"] -> printClients stdout
+      ["update",client,link] -> update stdout client link
+      ["update"] -> updateCurrentClient stdout
+      ["set",client,link] -> setClient stdout client link
+      _ -> T.putStrLn $ "Unknown command. Commands: clients, update <client> <stylesheet>, set <client> <stylesheet> (sets the current client/stylesheet), update (no args, uses current client)"
+
+dispatch :: Handle -> MVar [(Text,([Text],Maybe Handle))] -> Text -> URL -> [Text] -> IO ()
+dispatch h cs method url headers = do
+  logLn $ T.pack (show h) <> ": Client connected."
+  go [("bdo",bdo)
+     ,("links",links)
+     ,("poll",poll)]
+
+  where bdo = do
+          getJs (fromMaybe "localhost" (lookupHeader "host" headers)) >>= replyJs h
+          hClose h
+
+        poll = modifyClient (\(links,_) -> (links,Just h))
+
+        links = do
+          rest <- T.hGetContents h
+          case requestBody headers rest of
+            Nothing -> return ()
+            Just body -> case parsePost body of
+              Nothing -> return ()
+              Just params -> case lookup "links" params of
+                Nothing -> return ()
+                Just links -> do modifyClient (\(_links,handle) -> (T.lines links,handle))
+                                 logLn $ T.pack (show h) <> ": Links updated."
+                                 reply h [] "Links updated."
+          hClose h
+
+        go handlers =
+          case find ((`isPrefixOf` (url_path url)).fst) handlers of
+              Just (_,handle) -> handle
+              Nothing -> do
+                logLn $ T.pack (show h) <> ": Unhandled request: " <> T.pack (show url)
+                hClose h
+        referer = fromMaybe "any" $ lookupHeader "referer" headers
+        modifyClient f = modifyMVar_ cs (return . modify) where
+          modify xs = case lookup referer xs of
+                        Nothing -> (referer,f (mempty,Nothing)) : xs
+                        Just x -> (referer,f x) : filter ((/=referer).fst) xs
+
+logLn :: Text -> IO ()
+logLn = T.hPutStrLn stderr
+
+getJs :: Text -> IO Text
+getJs host = do
+  js <- getDataFileName "bdo.js" >>= T.readFile
+  return $
+    T.unlines [js
+              ,"bdo.host = " <> T.pack (show ("http://" <> host <> "/" :: Text)) <> ";"
+              ,"bdo.init();"
+              ]
+
+replyJs h = reply h [("Content-Type","text/javascript; charset=utf-8")]
