5c7c0b1556c49795bab33bde3ff44f061efe27e2
Chris Done <chrisdone@gmail.com>
2013-09-26 22:59:28 +0200

    Tweak

--- bdo.py
+++ bdo.py
@@ -7,8 +7,7 @@ class Bdo(sublime_plugin.TextCommand):
         output = subprocess.Popen(
             "echo " + cmd + " | nc -w 10 localhost 9090",
             shell=True, stdout=subprocess.PIPE).stdout.read()
-        print output
-        if len(output) > 0:
+        if len(output) > 0 and output != "Sending link update ...\n":
             view = sublime.active_window().new_file()
             edit = view.begin_edit()
             view.insert(edit, 0, output)
