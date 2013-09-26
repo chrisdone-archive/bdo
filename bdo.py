2b74fccbed0a63a503d59ac46fe90d0916abe39c
Chris Done <chrisdone@gmail.com>
2013-09-26 22:56:05 +0200

    Add sublime script

---
+++ bdo.py
@@ -0,0 +1,15 @@
+import sublime, sublime_plugin, subprocess, threading, time
+
+class Bdo(sublime_plugin.TextCommand):
+    def run(self, cmd):
+        sublime.active_window().show_input_panel("bdo ", "update", self.execute, None, None)
+    def execute(self, cmd):
+        output = subprocess.Popen(
+            "echo " + cmd + " | nc -w 10 localhost 9090",
+            shell=True, stdout=subprocess.PIPE).stdout.read()
+        print output
+        if len(output) > 0:
+            view = sublime.active_window().new_file()
+            edit = view.begin_edit()
+            view.insert(edit, 0, output)
+            view.end_edit(edit)
